f7db87b882abb50a56dd237dd85f513db18016fe
xs <xxx852925067@163.com>
2020-11-26 20:17:30 +0800

    init

---
+++ CPU/CPU.srcs/sources_1/new/inst_mem.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/11/26 20:05:41
+// Design Name: 
+// Module Name: inst_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module inst_mem(
+
+    );
+endmodule
